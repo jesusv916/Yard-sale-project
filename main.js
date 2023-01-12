43c88e832e8c9fbdf25197fe79e5fc2d30624a1f
jesusv916 <jevamon916@hotmail.com>
2023-01-11 20:06:07 -0500

    Integrating the aside shopping cart

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
 const mobileMenu = document.querySelector('.mobile-menu');
 const burguerMenu = document.querySelector('.menu');
 
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+menuShoppingCart.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu(){
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
-
 function toggleMobileMenu() {
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart(){
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
